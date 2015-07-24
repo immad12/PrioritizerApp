2a83422096690ee408cd63c4d012fdb54a2b0deb
immad12 <memad12@student.sdu.dk>
2015-07-24 19:24:04 +0200

    Implemented expandable listview, with working test data

--- app/src/main/java/dk/mlm/prioritizer/MainActivity.java
+++ app/src/main/java/dk/mlm/prioritizer/MainActivity.java
@@ -4,13 +4,87 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter expandableListAdapter;
+    private List<String> expandableListTitle;
+    private Map<String, List<String>> expandableListDetail = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton ibtnAddList = (ImageButton) findViewById(R.id.imageAddList);
+        ibtnAddList.setOnClickListener(this);
+
+        expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+
+        expandableListDetail = ExpandableListData.getData();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+
+        expandableListAdapter = new ExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(), "The " +
+                                expandableListTitle.get(groupPosition) + " list is expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(), "The " +
+                                expandableListTitle.get(groupPosition) + " list is collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                )
+                        .show();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Start new activity to create a list!
     }
 
     @Override
